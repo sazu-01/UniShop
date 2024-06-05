24a97a9e6bd0ddd2d8add25d46914742f9ccf5eb
sazu0101 <sazu2441@gmail.com>
2024-06-05 12:06:56 -0700

    add navbar navigation

---
+++ client/src/components/Navigation.tsx
@@ -0,0 +1,75 @@
+
+
+const Navigation = [
+    {
+        icon : "",
+        title : "Fashion",
+        href : "/fashion",
+        categorise : ["men" , "women" , "kid", "old" , "new arrival"]
+    },
+    {
+        icon : "",
+        title : "Electronics",
+        href : "/electronics",
+        categorise : ["mobile" , "laptop", "smart watch" , "monitor" , "play station 5",]
+    },
+
+    {
+        icon : "",
+        title : "Furniture",
+        href : "/furniture",
+        categorise : ["sofa" , "chair" , "living room", "table" , "new arrival"]
+    },
+
+    {
+        icon : "",
+        title : "Grocery",
+        href : "/grocery",
+        categorise : ["meat" , "fish" , "vegetables", "fruits",  "egg" ,]
+    },
+
+    {
+        icon : "",
+        title : "Medicine",
+        href : "/medicine",
+        categorise : ["health care" , "supplement" , "herbal", "baby & mom care" , "veterinary"]
+    },
+    {
+        icon : "",
+        title : "Pets",
+        href : "/pets",
+        categorise : ["food" , "dog collar" , "toys", "navbar 1" , "navbar 2"]
+    },
+    {
+        icon : "",
+        title : "Book",
+        href : "/book",
+        categorise : ["" , "" , "" , "" , ""]
+    },
+    {
+        icon : "",
+        title : "Demo Navbar",
+        href : "/demo-navbar-1",
+        categorise : ["" , "" , "", "" , ""]
+    },
+    {
+        icon : "",
+        title : "Demo Navbar 2",
+        href : "/demo-navbar-2",
+        categorise : ["" , "" , "", "" , ""]
+    },
+    {
+        icon : "",
+        title : "Demo Navbar 3",
+        href : "/demo-navbar-3",
+        categorise : ["" , "" , "", "" , ""]
+    },
+    {
+        icon : "",
+        title : "Demo Navbar 4",
+        href : "/demo-navbar-4",
+        categorise : ["" , "" , "", "" , ""]
+    },
+
+
+]
