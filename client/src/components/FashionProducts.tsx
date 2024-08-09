859de5721337a24731abeb68d49791aa8e67a6e5
coder-mizan01 <p.mizan2441@gmail.com>
2024-08-09 22:00:15 +0600

    add my old css on FashionShopProduct.tsx

--- client/src/components/FashionProducts.tsx
+++ client/src/components/FashionProducts.tsx
@@ -11,36 +11,39 @@ import { ProductType } from '../types/productSlicetypes';
 //css
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import "../css/FashionProducts.css";
 
 //icons 
 import { BsArrowRightShort, BsArrowLeftShort } from 'react-icons/bs';
-import { FaStar } from 'react-icons/fa';
+import { FaStar , FaStarHalfAlt  } from 'react-icons/fa';
+
 
 
 const FashionProducts = () => {
-  
-  function SampleNextArrow(props:any) {
-    const {className , style , onClick} = props;
-    return (
-      <BsArrowLeftShort
-      className={className}
-      style={{...style}}
-      onClick={onClick}
-      />
-    )
-  }
-
-  function SamplePrevArrow(props:any){
-    const {className,style,onClick} = props;
+  //make custome next arrow 
+  function SampleNextArrow(props: any) {
+    const { className, style, onClick } = props;
     return (
       <BsArrowRightShort
-      className={className}
-      style={{...style}}
-      onClick={onClick}
+        className={className}
+        style={{ ...style }}
+        onClick={onClick}
       />
     )
   }
 
+  //make custome previous arrow
+  function SamplePrevArrow(props: any) {
+    const { className, style, onClick } = props;
+    return (
+      <BsArrowLeftShort
+        className={className}
+        style={{ ...style }}
+        onClick={onClick}
+      />
+    )
+  }
+  //update settings of Slider & make responsive
   var settings = {
     dots: false,
     infinite: false,
@@ -83,15 +86,15 @@ const FashionProducts = () => {
         },
       },
       {
-       breakpoint : 670,
-       settings: {
-        slidesToShow : 2,
-        slidesToScroll : 2,
-        initialSlide : 1,
-        speed: 800,
-        dots : false,
-        infinit : false,
-       }
+        breakpoint: 670,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 1,
+          speed: 800,
+          dots: false,
+          infinit: false,
+        }
       },
       {
         breakpoint: 500,
@@ -100,7 +103,7 @@ const FashionProducts = () => {
           slidesToScroll: 2,
           speed: 800,
           dots: false,
-          infinit : false,
+          infinit: false,
         },
       },
       {
@@ -110,7 +113,7 @@ const FashionProducts = () => {
           slidesToScroll: 1,
           speed: 800,
           dots: false,
-          infinit : false,
+          infinit: false,
         },
       },
       {
@@ -120,56 +123,60 @@ const FashionProducts = () => {
           slidesToScroll: 1,
           speed: 800,
           dots: false,
-          infinit:false,
+          infinit: false,
         },
       },
     ],
   };
+  
+  //get products from product reducer 
+  const { products } = useAppSelector((state) => state.products);
 
-    const {products} = useAppSelector((state)=>state.products);
-    
   return (
     <>
-    <div className="slide-header">
-      <p>categroy name</p>
-      <Link to={`/`}>see all</Link>
-    </div>
+      {/*slide header part*/}
+      <div className="slide-header">
+        <p>categroy name</p>
+        <Link to={`/`}>see all</Link>
+      </div>
 
-    <Slider {...settings} className="">
+       {/*slider part*/}
+      <Slider {...settings} className="">
+        
+        {/*if porduct is not null and length is greate than 0*/}
+        {products && products.length > 0 ? (
+          products.map((pro: ProductType) => {
+            const { _id, title, slug, images, price } = pro;
+            return (
+              <div key={_id} className="product">
+                {/*product img & content part*/}
+                <Link to={`/product/${slug}`}>
 
-     { products && products.length > 0 ? (
-            products.map((pro:ProductType)=>{
-              const {_id,title,slug,images,price} = pro;
-              return (
-                <div key={_id} className="product">
-                  <Link to={`/product/${slug}`}>
-
-                   <div className="product-img">
+                  <div className="product-img">
                     <img src={`${images[0]}`} alt="" className="home-pro-img" />
-                   </div>
-
-                   <div className="pro-content">
-                    <p className="title">{title}</p>
-                    <p className="rating"><FaStar /><FaStar/></p>
                   </div>
 
-                  </Link>
+                  <div className="pro-content">
+                    <p className="title">{title}</p>
+                    <p className="rating"><FaStar /><FaStar /><FaStar /> <FaStarHalfAlt /><FaStarHalfAlt /></p>
+                  </div>
 
+                </Link>
+                
+                {/*product price & button part*/}
                 <div className="addcart">
-                  <p className='price'>{price}</p>
-                  <button>add to cart</button>
+                  <p className='price'>${price}</p>
+                  <button className="add-to-cart">+add</button>
                 </div>
-                 
 
-                  
-                </div>
-              )
-            })
-        ) : ( 
-        <div>no products loading</div>
+              </div>
+            )
+          })
+        ) : (
+          <div>no products loading</div>
         )}
-      
-    </Slider>
+
+      </Slider>
 
     </>
   )
