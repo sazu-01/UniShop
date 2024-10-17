437de6dd9d5a171c71363e57adbc37765e360de6
coder-mizan01 <p.mizan2441@gmail.com>
2024-10-17 14:07:52 +0600

    chore: configure environment variables in axios setup && add multiple origin in backend

--- client/src/utili/axiosConfig.tsx
+++ client/src/utili/axiosConfig.tsx
@@ -1,9 +1,18 @@
 //packages
 import axios from 'axios';
 
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL_PROD: string;
+  readonly VITE_BASE_URL_DEV: string;
+  readonly MODE: 'development' | 'production';
+}
+
+
 //custome instance of axios
 export const api = axios.create({
-    baseURL: 'https://unishop-server.onrender.com/api',//set the base url for all request
+    baseURL: import.meta.env.MODE === 'production' 
+    ? import.meta.env.VITE_BASE_URL_PROD 
+    : import.meta.env.VITE_BASE_URL_DEV,
     withCredentials: true, // To send cookies with requests
 });
 
