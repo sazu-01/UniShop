20927c6f0655a4d1bdaf3b52455096aac6294824
coder-mizan01 <p.mizan2441@gmail.com>
2024-10-25 01:30:35 +0600

    change DB.js and server.js code for solving deployment error

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,19 @@ import {serverPort} from "./hiddenEnv.js";
 
 
 // Start the Express application listening
-app.listen(serverPort, async function () {
-    console.log(`server running at http://localhost:${serverPort}`);
-    await ConnectDatabase()
-});
+const startServer = async () => {
+    try {
+      // Connect to database first
+      await ConnectDatabase();
+      
+      // Only start listening once database is connected
+      app.listen(serverPort, () => {
+        console.log(`Server running at http://localhost:${serverPort}`);
+      });
+    } catch (error) {
+      console.error('Failed to start server:', error);
+      process.exit(1);
+    }
+};
+
+startServer();
