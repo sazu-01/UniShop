b91498783c79090c47c875bcd4da1ad3085c279d
coder-mizan01 <p.mizan2441@gmail.com>
2025-02-02 17:43:08 +0600

    fixed api url

--- client/app/utili/axiosConfig.tsx
+++ client/app/utili/axiosConfig.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 //custome instance of axios
 export const api = axios.create({
-    baseURL : "http://unishop-backend.vercel.app/api",
+    baseURL : "https://unishop-backend.vercel.app/api",
     withCredentials: true, // To send cookies with requests
     headers : {
         "Content-Type" : "application/json"
