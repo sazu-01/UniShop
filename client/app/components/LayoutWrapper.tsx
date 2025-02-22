d914590275b98a89551a4988cfd225d2652ed0e3
coder-mizan01 <p.mizan2441@gmail.com>
2025-02-22 17:19:04 +0600

    remove multiple homepage and multiple homepages codes

--- client/app/components/LayoutWrapper.tsx
+++ client/app/components/LayoutWrapper.tsx
@@ -1,40 +1,21 @@
+"use client";
 
-'use client'
-
-import { usePathname } from "next/navigation";
-import HomeHeader from "../layouts/HomeHeader";
-import HomeCover from "../layouts/HomeCover";
-import Demos from "../layouts/Demos";
 import Header from "../layouts/Header";
 import MobileMenu from "../layouts/MobileMenu";
 import Footer from "../layouts/Footer";
 import { ReactNode } from "react";
 
-interface LayoutWrapperProps{
-    children : ReactNode
+interface LayoutWrapperProps {
+  children: ReactNode;
 }
 
-export default function LayoutWrapper({ children } : LayoutWrapperProps) {
-  const pathname = usePathname();
-  
+export default function LayoutWrapper({ children }: LayoutWrapperProps) {
   return (
     <>
-      {pathname === "/" ? (
-        <>
-          <HomeHeader />
-          <HomeCover />
-          <Demos />
-        </>
-      ) : (
-        <Header />
-      )}
+      <Header />
       {children}
-      {pathname === "/" ? null : (
-        <>
-          <MobileMenu />
-          <Footer />
-        </>
-      )}
+      <Footer />
+      <MobileMenu />
     </>
   );
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,12 @@
+import FashionProducts from "./components/FashionProducts";
+import FashionCover from "./layouts/FashionCover";
 
 
 export default function Home() {
   return (
     <>
-
-    <div className="">
-
-    </div>
+   <FashionCover /> 
+   <FashionProducts />
     </>
 
   );
