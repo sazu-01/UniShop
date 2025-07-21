639103f3e25de41dbc31966a62ffb2226c1e18d9
sazu-01 <p.mizan2441@gmail.com>
2025-07-22 04:30:42 +0600

    feat: add inner image zoom feature

--- client/app/components/Images.tsx
+++ client/app/components/Images.tsx
@@ -1,33 +1,51 @@
 
 'use client'
 
+//packages
+import InnerImageZoom from 'react-inner-image-zoom';
+
+//css
+import 'react-inner-image-zoom/lib/styles.min.css';
+
+
 //types
 import { ImageProps } from "@/app/types/props"
 import Image from "next/image"
 import { useState } from "react"
 
-const Images : React.FC<ImageProps> = ({ imgs }) => {
+
+const Images: React.FC<ImageProps> = ({ imgs }) => {
 
   const [viewImage, setViewImage] = useState(0)
 
-  const handleImageChange = (index : any) => {
+  const handleImageChange = (index: any) => {
     setViewImage(index)
   }
-  
+
   return (
     <>
       <div id="images">
         {/*view image */}
-        <Image className="img-fluid" src={imgs[viewImage]} alt="" width={500} height={500} />
+        <div className="container-fluid p-0" style={{ maxWidth: '100%', overflowX: 'auto' }}>
+          <InnerImageZoom
+            src={imgs[viewImage]}
+            zoomSrc={imgs[viewImage]}
+            zoomPreload={true}
+            zoomType="hover"
+            className="img-fluid w-100"
+            imgAttributes={{ alt: `${imgs[viewImage]}` }} 
+          />
+        </div>
+
         {/*thumbnails image */}
         <div className="thumbnails">
-          {imgs.map((img, index)=>(
-            <Image key={img} className="thumb-img img-fluid" 
-             onMouseOver={()=>handleImageChange(index)}
-             src={imgs[index]} alt="" width={500} height={500} 
-             />
+          {imgs.map((img, index) => (
+            <Image key={img} className="thumb-img img-fluid"
+              onMouseOver={() => handleImageChange(index)}
+              src={imgs[index]} alt="" width={500} height={500}
+            />
           ))}
-          
+
         </div>
       </div>
 
