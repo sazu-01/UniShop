bce6871974fca6410c2017da65f39da4e78523d9
sazu-01 <p.mizan2441@gmail.com>
2025-09-10 01:10:40 +0600

    mend

--- server/src/app.js
+++ server/src/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin : ["http://localhost:3000", "https://unishopbd.vercel.app"],
+    origin : ["http://localhost:3000", "https://www.pacyficshop.com"],
     credentials  : true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
